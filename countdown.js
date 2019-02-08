090aad20f478d6c8de857a789855d3bdfd63dd5b
Bindu Prakash <bindu2305@gmail.com>
2019-02-08 08:00:27 +0000

    closure assignment

---
+++ countdown.js
@@ -0,0 +1,15 @@
+var countdownGenerator = function (x) {
+  var countDown = x;
+
+  return function(){
+    countDown--;
+  };
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
